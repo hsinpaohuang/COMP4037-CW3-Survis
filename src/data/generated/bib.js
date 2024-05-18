5d5e65d013037dee18ac4d79692b3e0dc2474fa4
hsinpaohuang <hsinpaohuang@gmail.com>
2024-05-18 06:26:12 +0100

    add references

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,113 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
+    "4036381": {
+        "author": "Bruck, Jehoshua and Gao, Jie and Jiang, Anxiao",
+        "booktitle": "2006 IEEE International Symposium on Information Theory",
+        "doi": "10.1109/ISIT.2006.261978",
+        "keywords": "Information filtering;Information filters;Frequency;Computer science;Data structures;Statistical distributions;Data compression;Application software;Combinatorial mathematics;Web server;Bloom Filter;Membership Query;Combinatorics",
+        "number": "",
+        "pages": "2304-2308",
+        "title": "Weighted Bloom filter",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2006"
+    },
+    "4698251": {
+        "author": "Ahmadi, Mahmood and Wong, Stephan",
+        "booktitle": "IEEE GLOBECOM 2008 - 2008 IEEE Global Telecommunications Conference",
+        "doi": "10.1109/GLOCOM.2008.ECP.476",
+        "keywords": "Filters;Counting circuits;Data structures;Distributed databases;Delay;Laboratories;Mathematics;Computer science;Hardware;Computer architecture",
+        "number": "",
+        "pages": "1-5",
+        "title": "A Memory-Optimized Bloom Filter Using an Additional Hashing Function",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2008"
+    },
+    "6195563": {
+        "author": "Rottenstreich, Ori and Kanizo, Yossi and Keslassy, Isaac",
+        "booktitle": "2012 Proceedings IEEE INFOCOM",
+        "doi": "10.1109/INFCOM.2012.6195563",
+        "keywords": "Radiation detectors;Memory management;Filtering algorithms;Complexity theory;Hardware;Arrays;Encoding",
+        "number": "",
+        "pages": "1880-1888",
+        "title": "The Variable-Increment Counting Bloom Filter",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2012"
+    },
+    "6877748": {
+        "author": "Tapolcai, J\u00e1nos and B\u00edr\u00f3, J\u00f3zsef and Babarczi, P\u00e9ter and Guly\u00e1s, Andr\u00e1s and Heszberger, Zal\u00e1n and Trossen, Dirk",
+        "doi": "10.1109/TNET.2014.2342155",
+        "journal": "IEEE/ACM Transactions on Networking",
+        "keywords": "Topology;IP networks;Network topology;Scalability;Routing;Encoding;Internet;Bloom filter;information-centric networking;multicast addressing;source routing",
+        "number": "6",
+        "pages": "1832-1845",
+        "title": "Optimal False-Positive-Free Bloom Filter Design for Scalable Multicast Forwarding",
+        "type": "ARTICLE",
+        "volume": "23",
+        "year": "2015"
+    },
+    "7264999": {
+        "author": "Lim, Hyesook and Lee, Jungwon and Yim, Changhoon",
+        "doi": "10.1109/LCOMM.2015.2478462",
+        "journal": "IEEE Communications Letters",
+        "keywords": "Simulation;Probability;Memory management;Programming;Computers;Information filters;Bloom filter;complement Bloom filter;true positive;false positive;negative;Bloom filter;complement bloom filter;true positive;false positive;negative",
+        "number": "11",
+        "pages": "1905-1908",
+        "title": "Complement Bloom Filter for Identifying True Positiveness of a Bloom Filter",
+        "type": "ARTICLE",
+        "volume": "19",
+        "year": "2015"
+    },
+    "7404748": {
+        "author": "Lu, Jianyuan and Yang, Tong and Wang, Yi and Dai, Huichen and Jin, Linxiao and Song, Haoyu and Liu, Bin",
+        "booktitle": "2015 IEEE 23rd International Symposium on Quality of Service (IWQoS)",
+        "doi": "10.1109/IWQoS.2015.7404748",
+        "keywords": "Filtering theory;Filtering algorithms;Cryptography;Quality of service;Memory management;System performance;Standards",
+        "number": "",
+        "pages": "289-298",
+        "title": "One-hashing bloom filter",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2015"
+    },
+    "851975": {
+        "author": "Li Fan and Pei Cao and Almeida, J. and Broder, A.Z.",
+        "doi": "10.1109/90.851975",
+        "journal": "IEEE/ACM Transactions on Networking",
+        "keywords": "Cache memory;Internet;Bandwidth;Computer science;Routing protocols;Telecommunication traffic;Particle measurements;Virtual prototyping;Bit rate;Data analysis",
+        "number": "3",
+        "pages": "281-293",
+        "title": "Summary cache: a scalable wide-area Web cache sharing protocol",
+        "type": "ARTICLE",
+        "volume": "8",
+        "year": "2000"
+    },
+    "carrea2016yesno": {
+        "archiveprefix": "arXiv",
+        "author": "Laura Carrea and Alexei Vernitski and Martin Reed",
+        "eprint": "1603.01060",
+        "primaryclass": "cs.DS",
+        "title": "Yes-no Bloom filter: A way of representing sets with fewer false positives",
+        "type": "misc",
         "year": "2016"
+    },
+    "donnet2006retouched": {
+        "archiveprefix": "arXiv",
+        "author": "Benoit Donnet and Bruno Baynat and Timur Friedman",
+        "eprint": "cs/0607038",
+        "primaryclass": "cs.NI",
+        "title": "Retouched Bloom Filters: Allowing Networked Applications to Flexibly Trade Off False Positives Against False Negatives",
+        "type": "misc",
+        "year": "2006"
+    },
+    "goodrich2015invertible": {
+        "archiveprefix": "arXiv",
+        "author": "Michael T. Goodrich and Michael Mitzenmacher",
+        "eprint": "1101.2245",
+        "primaryclass": "cs.DS",
+        "title": "Invertible Bloom Lookup Tables",
+        "type": "misc",
+        "year": "2015"
     }
 };
